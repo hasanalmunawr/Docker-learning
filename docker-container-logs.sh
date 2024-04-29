1b85cf6d1a74689fea932fbab939e0d3acc967e5
hasanalmunawr <hasanalmunawar01@gmail.com>
2024-04-29 14:12:51 +0700

    push aja

--- docker-container-logs.sh
+++ docker-container-logs.sh
@@ -4,9 +4,9 @@ sudo docker container create --name contohpostgres postgres:lastest
 sudo docker container start contohpostgres
 
 # SEE THE LOGS OF CONTAINER IS RUNNING
-#sudo docker container logs "nama_container"
+#sudo docker container logs "CONTAINER_NAME"
 sudo docker container logs contohpostgres
 
 # SEE THE LOGS OF CONTAINER IS RUNNING AND WAITING NEW LOG
-#sudo docker container logs -f "nama_container"
+#sudo docker container logs -f "CONTAINER_NAME"
 sudo docker container logs -f contohpostgres
