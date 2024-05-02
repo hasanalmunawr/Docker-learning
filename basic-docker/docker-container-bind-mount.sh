7fd3eef1614bd8c9fd583a57d007f2f2c29224b2
hasanalmunawr <hasanalmunawar01@gmail.com>
2024-05-02 20:49:30 +0700

    push aja

--- basic-docker/docker-container-bind-mount.sh
+++ basic-docker/docker-container-bind-mount.sh
@@ -4,5 +4,13 @@
 
 docker container create --name postgresdate --mount "type=bind, source=/home/hasanalmunawr/Devlopment/Docker/belajar-docker-dasar/basic-docker/postgres-data, destination=/data/pgdata" --publish 1234:5432 postgres:latest
 
-docker container create --name datapostgres --publish 5431:5432 --env POSTGRES_USER=hasan --env POSTGRES_PASSWORD=password -d postgres
+docker container create --name datapostgres --mount "type=bind, source=/home/hasanalmunawr/Devlopment/Docker/belajar-docker-dasar/basic-docker/postgres-data, destination=/data/pgdata" --publish 5431:5432 --env POSTGRES_USER=hasan --env POSTGRES_PASSWORD=password postgres:latest
 
+# THE CORRECT VERSION FROM GEMINI AI
+#docker container create --name datapostgres \
+#  -v /home/hasanalmunawr/Devlopment/Docker/belajar-docker-dasar/basic-docker/postgres-data:/data/pgdata \
+#  -p 5431:5432 \
+#  -e POSTGRES_USER=hasan \
+#  -e POSTGRES_PASSWORD=password \
+POSTGRES_DB: users
+#  postgres:latest
