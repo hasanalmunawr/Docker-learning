28bcada9b16ba6b33d366e8eec3da7f847cf962a
hasanalmunawr <hasanalmunawar01@gmail.com>
2024-05-04 10:31:00 +0700

    push aja

---
+++ basic-docker/docker-inspect.sh
@@ -0,0 +1,13 @@
+# INSPECT IS USING TO SEE IN DETAIL
+
+# TO SEE DETAILS OF IMAGE
+docker image inspect image_name
+
+# TO SEE DETAILS OF CONTAINER
+docker container inspect container_name
+
+# TO SEE DETAILS OF VOLUME
+docker volume inspect volume_name
+
+# TO SEE DETAILS OF NETWORK
+docker network inspect network_name
